1024ab85837ad25533ce9cd1f13757f2a3f43f5f
fallenleaveslikewearyButterfly <30828851+fallenleaveslikewearyButterfly@users.noreply.github.com>
2020-09-23 15:07:58 +0800

    Create leetcode90.scala

---
+++ src/main/scala/com/train/leetcode/backtrace/leetcode90.scala
@@ -0,0 +1,30 @@
+package com.train.leetcode.backtrace
+
+import scala.collection.mutable.{ListBuffer}
+
+object leetcode90 {
+
+  val res = ListBuffer[List[Int]]()
+  val rr = ListBuffer[Int]()
+
+  def _subsetsWithDup(index:Int,nums: Array[Int]): Unit = {
+    res.append(rr.toList)
+
+    for(i<-Range(index,nums.length) if !(i>index && nums(i)==nums(i-1))){
+      rr.append(nums(i))
+      _subsetsWithDup(i+1,nums)
+      rr.trimEnd(1)
+    }
+  }
+
+  def subsetsWithDup(nums: Array[Int]): List[List[Int]] = {
+    res.clear()
+    rr.clear()
+    _subsetsWithDup(0,nums.sorted)
+    res.toList
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(subsetsWithDup(Array(1,2,2)))
+  }
+}
