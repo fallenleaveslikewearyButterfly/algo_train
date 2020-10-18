6ffc83b1aa487c735d3e269c511d18aeb5c5bba6
lilan <1063972142@qq.com>
2020-10-18 22:19:24 +0800

    add BubbleSort

---
+++ src/main/scala/com/train/Sorter/MergeSort.scala
@@ -0,0 +1,14 @@
+package com.train.Sorter
+
+/**
+  * @author: Lilan
+  * @date: 2020/10/18 18:56
+  */
+object MergeSort {
+
+
+  def _mergeSort[E <% Ordered[E]](arr:Array[E], n:Int): Unit ={
+
+  }
+
+}
---
+++ src/main/scala/com/train/Sorter/ShellSort.scala
@@ -0,0 +1,30 @@
+package com.train.Sorter
+
+import com.train.common.SortTestHelper
+
+/**
+  * @author: Lilan
+  * @date: 2020/10/18 18:56
+  */
+object ShellSort {
+  def shellSort[E <% Ordered[E]](arr:Array[E], n:Int): Unit ={
+
+    var gap=n/2
+
+    while(gap>0){
+      for(i<-Range(gap,n)){
+        var e = arr(i)
+        var j = i
+        while(j-gap>0 && (arr(j-gap) > e)){
+          arr(j)=arr(j-gap)
+          j=j-gap
+        }
+        if(j-gap>0){
+          arr(j-gap)=e
+        }
+      }
+      gap = gap/2
+    }
+
+  }
+}
