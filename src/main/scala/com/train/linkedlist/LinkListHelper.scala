0068948eab07d356a5bba4fa9fe0d2dc984d5d64
fallenleaveslikewearyButterfly <30828851+fallenleaveslikewearyButterfly@users.noreply.github.com>
2020-10-20 14:10:52 +0800

    Create LinkListHelper.scala

---
+++ src/main/scala/com/train/linkedlist/LinkListHelper.scala
@@ -0,0 +1,35 @@
+package com.train.linkedlist
+
+class ListNode(_x: Int = 0, _next: ListNode = null) {
+  var next: ListNode = _next
+  var x: Int = _x
+}
+
+object LinkListHelper {
+  def mkLinkList(arr:Array[Int]):ListNode={
+    var current = new ListNode()
+    val head = new ListNode()
+    head.next=current
+    for(i<-Range(0,arr.length-1)){
+      current.x = arr(i)
+      current.next = new ListNode()
+      current=current.next
+    }
+    current.x = arr(arr.length-1)
+    head
+  }
+
+  def printLinkedList(head:ListNode):Unit={
+    var current = head.next
+    while(current!=null){
+      print(f"->${current.x}")
+      current = current.next
+    }
+    print("->null")
+  }
+
+  def main(args: Array[String]): Unit = {
+    val head = mkLinkList(Array(1,2,3,4,5))
+    printLinkedList(head)
+  }
+}
