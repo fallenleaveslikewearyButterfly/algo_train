5cd6380e5e24c783bdb36d0390dd0a5a16cb48a4
fallenleaveslikewearyButterfly <30828851+fallenleaveslikewearyButterfly@users.noreply.github.com>
2020-10-22 14:41:39 +0800

    Update leetcode1441.scala

--- src/main/scala/com/train/stack/leetcode1441.scala
+++ src/main/scala/com/train/stack/leetcode1441.scala
@@ -2,15 +2,29 @@ package com.train.stack
 import scala.collection.mutable.ArrayStack
 
 object leetcode1441 {
+
   val stack = ArrayStack[String]()
+
   def buildArray(target: Array[Int], n: Int): List[String] = {
     stack.clear()
+    var targetIndex = 0
 
-    var index = 1
+    var end  =  if(target(target.length-1)<n) target(target.length-1) else n
+    for(index<-Range(1,end+1)) {
+      if (target(targetIndex) == index) {
+        stack.push("Push")
+        targetIndex += 1
+      } else {
+        stack.push("Push")
+        stack.push("Pop")
+      }
+    }
+
     stack.toList.reverse
   }
 
   def main(args: Array[String]): Unit = {
-    println(buildArray(Array(1,3),3))
+    println(buildArray(Array(2,3,4),4))
   }
 }
+
