56ca6a80d899dfb8fbd5007b1a08e90b4e067343
fallenleaveslikewearyButterfly <30828851+fallenleaveslikewearyButterfly@users.noreply.github.com>
2020-11-03 13:15:38 +0800

    Create leetcode148.scala

---
+++ src/main/scala/com/train/linkedlist/leetcode148.scala
@@ -0,0 +1,46 @@
+package com.train.linkedlist
+
+//正常思路是使用归并排序
+//该版本为插入排序
+object leetcode148 {
+  def sortList(head: ListNode): ListNode = {
+    var dummyHead = new ListNode()
+    dummyHead.next=head
+    if(head==null){
+      return head
+    }
+    var l1tail = head
+    var l2 = head.next
+    head.next = null
+
+    while(l2!=null){
+      if(l2.x<l1tail.x){
+        var pre = dummyHead
+        var cur = dummyHead.next
+        while(cur.x<=l2.x){
+          pre = cur
+          cur = cur.next
+        }
+        var t=l2.next
+        pre.next = l2
+        l2.next = cur
+        l2=t
+      }else{
+        l1tail.next=l2
+        l2 = l2.next
+        l1tail=l1tail.next
+        l1tail.next=null
+      }
+
+    }
+    return dummyHead.next
+  }
+
+  def main(args: Array[String]): Unit = {
+    LinkListHelper.printLinkedList(sortList(LinkListHelper.mkLinkList(Array(2,4,5,4)).next))
+    LinkListHelper.printLinkedList(sortList(LinkListHelper.mkLinkList(Array(4,2,1,3)).next))
+    LinkListHelper.printLinkedList(sortList(LinkListHelper.mkLinkList(Array(-1,5,3,4,0)).next))
+  }
+
+
+}
