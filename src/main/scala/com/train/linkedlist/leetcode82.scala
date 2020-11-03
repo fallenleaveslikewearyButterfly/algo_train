939f42db4f192d1b253bc94001ed76f092b7e868
fallenleaveslikewearyButterfly <30828851+fallenleaveslikewearyButterfly@users.noreply.github.com>
2020-11-03 09:59:24 +0800

    Update leetcode82.scala

--- src/main/scala/com/train/linkedlist/leetcode82.scala
+++ src/main/scala/com/train/linkedlist/leetcode82.scala
@@ -2,17 +2,31 @@ package com.train.linkedlist
 
 object leetcode82 {
   def deleteDuplicates(head: ListNode): ListNode = {
-    var pre:ListNode = null
+    val dummyHead = new ListNode()
+    dummyHead.next = head
+    var pre = dummyHead
     var cur = head
     while(cur!=null && cur.next!=null){
-
+      var count = 0
+      var t = cur.x
+      while(cur.next!=null && cur.next.x==t){
+        count +=1
+        cur = cur.next
+      }
+      if(count!=0){
+        pre.next = cur.next
+        cur = cur.next
+      }else{
+        pre = cur
+        cur = cur.next
+      }
     }
-    return head
+    return dummyHead.next
   }
 
   def main(args: Array[String]): Unit = {
     LinkListHelper.printLinkedList(deleteDuplicates(LinkListHelper.mkLinkList(Array(1,2,3,3,4,4,5)).next))
-    LinkListHelper.printLinkedList(deleteDuplicates(LinkListHelper.mkLinkList(Array(1,1,2)).next))
-    LinkListHelper.printLinkedList(deleteDuplicates(LinkListHelper.mkLinkList(Array(1,2,2)).next))
+    LinkListHelper.printLinkedList(deleteDuplicates(LinkListHelper.mkLinkList(Array(1,1,1,2,3)).next))
+    LinkListHelper.printLinkedList(deleteDuplicates(LinkListHelper.mkLinkList(Array(1,1)).next))
   }
 }
